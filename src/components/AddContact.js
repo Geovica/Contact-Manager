9fc7ae231056418575af581456750424e6c63f30
Geovica <fgeovic@gmail.com>
2021-07-14 16:59:12 +0800

    change width

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -19,8 +19,8 @@ class AddContact extends React.Component {
     };
     render() {
         return (
-            <div>
-                <h2 className="block text-gray-700 text-l  font-bold mb-2 pl-4">Add Contact</h2>
+            <div className="w-80">
+                <h2 className="block text-gray-700 text-l  font-bold mb-2 pl-4">Add New Contact</h2>
                 <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={this.add}>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
