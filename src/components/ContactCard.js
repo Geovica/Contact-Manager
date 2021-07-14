54a489c987a54587932ed0981439c77d4d8a1ab5
Geovica <fgeovic@gmail.com>
2021-07-14 14:31:11 +0800

    adding Links

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,27 +1,42 @@
 import React from "react";
-import transIcon from '../images/trash.svg';
+import { Link } from "react-router-dom";
+import trashIcon from '../images/trash.svg';
+import editIcon from '../images/edit.svg';
 import userIcon from '../images/user.png';
 const ContactCard = (props) => {
-    const { id, name, email} = props.contact;
+    const { id, name, email } = props.contact;
     return (
-     
+
         <div className="text-sm text-gray-500">
-        <div className="grid grid-cols-2 gap-4 p-4">
-            <div className="flex"> 
-            <img src={userIcon} height={30} width={50} ></img> 
-            <div>
-            <h2>{name}</h2>
-            <h3 className="text-xs font-light   ">{email}</h3>
+            <div className="grid grid-cols-2 gap-4 p-4">
+                <Link to={{ pathname: `/contact/${id}`, state: { contact: props.contact } }}>
+                    <div className="flex">
+                        <img alt="user" src={userIcon} height={30} width={50} ></img>
+                        <div>
+
+                            <h2>{name}</h2>
+                            <h3 className="text-xs font-light">{email}</h3>
+
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to={{ pathname: `/edit/${id}`, state: { contact: props.contact } }}>
+                    <img alt="edit" src={editIcon}
+                        height={20}
+                        width={20}
+                        className="col-end-7 col-span-2 "
+                        onClick={() => props.clickHandler(id)} />
+                </Link>
+
+                <img alt="trash" src={trashIcon}
+                    height={20}
+                    width={20}
+                    className="col-end-7 col-span-2 "
+                    onClick={() => props.clickHandler(id)} />
             </div>
-            </div>
-            <img src={transIcon}
-                height={20}
-                width={20}
-                className="col-end-7 col-span-2 "
-                onClick={() => props.clickHandler(id)}/>
+            <hr className="shadow-sm" />
         </div>
-        <hr  className="shadow-sm" />
-    </div>
     )
 }
 
