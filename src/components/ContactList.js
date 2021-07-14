a1f4cdcf2ba63bcd9316d686d5498854f67c95e1
Geovica <fgeovic@gmail.com>
2021-07-14 16:58:13 +0800

    added contactList

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -19,12 +19,12 @@ const ContactList = (props) => {
       
     });
     return (
-        <div className="bg-blend-darken p-4 shadow text-gray-700 font-bold mb-5">
+        <div className="bg-blend-darken p-4 shadow text-gray-700 font-bold mb-5 w-96">
           
             <h1 className="pl-4 pb-4 col-span-2 text-lg">Contact List
             </h1>
             <Link to="/add">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold rounded p-1 ml-4">Add Contact</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold rounded p-1 ml-4">Add New Contact</button>
             </Link>
   
             {renderContactList}
