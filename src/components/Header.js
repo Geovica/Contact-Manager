90eed71008422609dd9428fc6c50e68034aa0c96
Geovica <fgeovic@gmail.com>
2021-07-14 08:47:05 +0800

    adding header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <div className="bg-blend-darken p-4 shadow text-gray-700 text-xl font-bold mb-5">
+                <h2>Contact Manager</h2>
+            </div>
+    )
+};
+export default Header;
