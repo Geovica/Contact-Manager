e4d61323db86ff36ff27d4fac2f70f4a8e93a104
Geovica <fgeovic@gmail.com>
2021-07-14 14:31:49 +0800

    adding contact list Link

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ContactCard from './ContactCard';
 
 const ContactList = (props) => {
@@ -7,13 +8,25 @@ const ContactList = (props) => {
     const deleteContactHandler =(id) => {
         props.getContactId(id);
     }
+
+    const contacts = [{
+        id: "1",
+        name: "Geovic",
+        email: "fgeovic@gmail.com"
+    }]
     const renderContactList = props.contacts.map((contact) => {
         return <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id} />
       
     });
     return (
-        <div className="bg-blend-darken p-4 shadow text-gray-700 text-l font-bold mb-5">
-            <h1 className="pl-4 pb-4">List</h1>
+        <div className="bg-blend-darken p-4 shadow text-gray-700 font-bold mb-5">
+          
+            <h1 className="pl-4 pb-4 col-span-2 text-lg">Contact List
+            </h1>
+            <Link to="/add">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold rounded p-1 ml-4">Add Contact</button>
+            </Link>
+  
             {renderContactList}
         </div>
     )
